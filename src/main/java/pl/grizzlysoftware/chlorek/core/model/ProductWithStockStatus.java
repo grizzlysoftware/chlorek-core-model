a4d83780d57fd03c070c40f391134d7a4799462c
bpawlowski <bpawlowski@grizzlysoftware.pl>
2021-11-17 15:09:51 +0100

    chore: tmp

--- src/main/java/pl/grizzlysoftware/chlorek/core/model/ProductWithStockStatus.java
+++ src/main/java/pl/grizzlysoftware/chlorek/core/model/ProductWithStockStatus.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
  */
 public class ProductWithStockStatus extends Product {
     public Integer lastInventoryValue;
-    public Integer stockQuantity;
+    public BigDecimal stockQuantity;
     public BigDecimal lastPurchaseNetPrice;
     public Long warehouseId;
 }
